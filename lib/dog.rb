ea801ef153bf84451d3d8ccad8482484c96ed5d4
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-16 20:39:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,20 @@
 class Dog
 
-  def initialize(dogsname, withbreed)
+  def name=(dogsname)
     @name = dogsname
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(withbreed)
     @breed = withbreed
   end
 
-  # def name=(dogsname)
-  #   @name = dogsname
-  # end
-  #
-  # def name
-  #   @name
-  # end
+  def breed
+    @breed 
+  end
 
 
 
